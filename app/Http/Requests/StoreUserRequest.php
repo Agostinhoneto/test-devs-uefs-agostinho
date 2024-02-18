5ffd20946e2dab154e338b9a0c5d5921eda86fb1
Agostinhoneto <agostneto6@gmail.com>
2024-02-18 09:51:49 -0300

    refatorando e corrigindo bugs

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -22,7 +22,7 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
             return [
-               // 'name' => 'required|string',
+                'name' => 'required|string|max:255',
                 'email' => 'required|email|unique:users,email',
                 'password' => 'required|min:6',
             ];
